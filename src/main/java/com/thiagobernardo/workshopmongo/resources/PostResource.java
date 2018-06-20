568cd4e4ce3ae4d12baba9c1709bfa55be3f1fb3
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-06-20 13:14:41 -0300

    Consulta com vários criterios

--- src/main/java/com/thiagobernardo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/thiagobernardo/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.thiagobernardo.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		String decodedText = URL.decodeParam(text);
 		List<Post> posts = service.findByTitle(decodedText);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 	
--- src/main/java/com/thiagobernardo/workshopmongo/resources/util/URL.java
+++ src/main/java/com/thiagobernardo/workshopmongo/resources/util/URL.java
@@ -2,8 +2,12 @@ package com.thiagobernardo.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 public class URL {
@@ -13,6 +17,16 @@ public class URL {
 			return URLDecoder.decode(s, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
 		}
 	}
 	
